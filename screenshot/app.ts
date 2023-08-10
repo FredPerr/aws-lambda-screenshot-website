ef4955219bab8727b84e46ed8324f3a12d5e6921
Frédéric Perron <frederic.perr@gmail.com>
2023-08-10 18:26:25 -0400

    feat: working screenshot

--- screenshot/app.ts
+++ screenshot/app.ts
@@ -1,39 +1,57 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import Chromium from '@sparticuz/chromium';
 import puppeteer from 'puppeteer-core';
+import { z } from 'zod';
+
+const endpoint_scheme = z.object({
+    url: z.string().url(),
+    width: z.number().min(300),
+    height: z.number().min(300),
+    fullscreen: z.boolean().optional(),
+});
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        if (!event.body)
+            throw new Error('You must provide a body (url, width, height, [fullscreen]) to use the function');
+
+        const params = endpoint_scheme.safeParse(JSON.parse(event.body));
+        if (!params.success) throw new Error('Could not parse the parameters given to the function');
+
+        const { url, width, height, fullscreen } = params.data;
+
         const browser = await puppeteer.launch({
             args: [...Chromium.args],
-            defaultViewport: Chromium.defaultViewport,
+            defaultViewport: {
+                width: width,
+                height: height,
+            },
             executablePath: await Chromium.executablePath('/opt/nodejs/node_modules/@sparticuz/chromium/bin'),
             headless: Chromium.headless,
         });
 
-        const url = 'https://www.google.com';
         const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'networkidle2', timeout: 12000 });
 
         const screenshot = await page.screenshot({
             type: 'jpeg',
-            fullPage: false,
+            fullPage: fullscreen ? true : false,
         });
-        const title = await page.title();
 
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                message: title,
-                screenshot: screenshot,
-            }),
+            headers: {
+                'content-type': 'application/octet-stream',
+            },
+            body: JSON.stringify(screenshot),
         };
     } catch (err) {
         console.log(err);
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'some error happened',
+                message: 'An error occurred while processing your request',
+                error_msg: err ? err : 'Unknown',
             }),
         };
     }
